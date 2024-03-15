bdb1b99faf42db1c1f8a2c687129c8204ed6e382
Ezdras <132713544+Ezdras01@users.noreply.github.com>
2024-03-15 09:04:31 -0600

    decimales
    Se agregó la función en la división para que solo muestre hasta 3 decimales.

--- functs.py
+++ functs.py
@@ -9,7 +9,9 @@ def multiplicacion( num1, num2):
     return num1*num2
 
 def division( num1, num2):
+    
     if num2 == 0:
         return "No se puede dividir entre 0"
     else:
-        return num1/num2
+        resultado = round(num1/num2, 3)
+        return resultado
